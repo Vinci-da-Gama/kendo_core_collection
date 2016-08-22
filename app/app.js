73b123958b6e17eb22cab0c0f7c51c523153ad99
c++gccOc <kelin.liao@csbengage.com>
2016-08-22 10:51:39 +1000

    init kendo

---
+++ app/app.js
@@ -0,0 +1,27 @@
+(function () {
+
+	var anguNg = ['ngAria', 'ngSanitize', 'ngAnimate', 'ngMessages', 'ngNotify', 'ngMask'];
+	var anguEx = ['ui.bootstrap', 'mgcrea.ngStrap', 'angularMoment', 'bootstrapLightbox'];
+	var routerCtrl = ['appname.router', 'appname.ctrl'];
+	var cons = ['appname.constant'];
+	var serv = ['appname.sig.service', 'appname.service', 'appname.promise.factory'];
+	var dir = ['appname.dir', 'appname.cust.dir'];
+
+	var depedencyArr = anguNg.concat(anguEx, routerCtrl, cons, serv, dir);
+	/**
+	* appname Module
+	*
+	* The main module of this application...
+	*/
+	angular.module('appname', depedencyArr);
+
+	angular.module('appname.router', ['ui.router']);
+	angular.module('appname.constant', []);
+	angular.module('appname.sig.service', []);
+	angular.module('appname.service', []);
+	angular.module('appname.promise.factory', []);
+	angular.module('appname.ctrl', []);
+	angular.module('appname.dir', ['appname.service', 'appname.sig.service']);
+	angular.module('appname.cust.dir', ['appname.service', 'appname.sig.service']);
+
+})();
---
+++ app/factory/promise-factory.js
@@ -0,0 +1,6 @@
+(function () {
+	var promiseM = angular.module('appname.promise.factory');
+
+	// promiseM
+
+})();
